1deadc6b315272e4c14d9b5010b16e3ece0c27e8
Endy Muhardin <endy.muhardin@gmail.com>
2011-07-12 13:24:58 +0700

    entity class pengajuan pinjaman

---
+++ aplikasi-kasbon-ejb/src/java/kasbon/entity/StatusPengajuan.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kasbon.entity;
+
+/**
+ *
+ * @author endy
+ */
+public enum StatusPengajuan {
+    DIAJUKAN,DISETUJUI,DITOLAK
+}
