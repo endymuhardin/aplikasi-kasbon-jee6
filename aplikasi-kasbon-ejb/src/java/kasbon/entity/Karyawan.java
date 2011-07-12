96b9f3b238767df6118175675ac9eeacf4e93099
Endy Muhardin <endy.muhardin@gmail.com>
2011-07-12 13:46:22 +0700

    menambahkan unique constraint di kolom nip

--- aplikasi-kasbon-ejb/src/java/kasbon/entity/Karyawan.java
+++ aplikasi-kasbon-ejb/src/java/kasbon/entity/Karyawan.java
@@ -9,12 +9,15 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 
 /**
  *
  * @author endy
  */
-@Entity
+@Entity 
+@Table(uniqueConstraints=@UniqueConstraint(columnNames={"nip"}))
 public class Karyawan implements Serializable {
     private static final long serialVersionUID = 1L;
     @Id
