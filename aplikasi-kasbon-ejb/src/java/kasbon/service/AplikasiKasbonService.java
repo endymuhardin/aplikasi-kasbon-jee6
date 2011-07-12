12faf1c6f790e8d783ef30c6639dc6f439c17bfc
Endy Muhardin <endy.muhardin@gmail.com>
2011-07-12 13:46:43 +0700

    tambah fitur berkaitan dengan entity karyawan

--- aplikasi-kasbon-ejb/src/java/kasbon/service/AplikasiKasbonService.java
+++ aplikasi-kasbon-ejb/src/java/kasbon/service/AplikasiKasbonService.java
@@ -4,6 +4,7 @@
  */
 package kasbon.service;
 
+import java.util.List;
 import javax.ejb.Stateless;
 import javax.ejb.LocalBean;
 import javax.persistence.EntityManager;
@@ -24,4 +25,20 @@ public class AplikasiKasbonService {
         em.persist(karyawan);
     }
 
+    public Karyawan findKaryawanById(Long id){
+        return em.find(Karyawan.class, id);
+    }
+    
+    public List<Karyawan> findAllKaryawan(){
+        return em.createQuery("select k from Karyawan order by k.nip")
+                .getResultList();
+    }
+    
+    public List<Karyawan> findKaryawanByNama(String nama){
+        return em.createQuery("select k from Karyawan "
+                + "where k.nama like :nama "
+                + "order by k.nip")
+                .setParameter("nama", "%"+nama+"%")
+                .getResultList();
+    }
 }
