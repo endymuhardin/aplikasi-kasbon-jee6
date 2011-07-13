82e45938a49feb42a1455b9f976af2b33b6c306f
Endy Muhardin <endy.muhardin@gmail.com>
2011-07-13 14:06:55 +0700

    bind input field ke object, masih error karena belum bikin converter

--- aplikasi-kasbon-war/src/java/kasbon/controller/PengajuanController.java
+++ aplikasi-kasbon-war/src/java/kasbon/controller/PengajuanController.java
@@ -9,6 +9,7 @@ import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 import kasbon.entity.Karyawan;
+import kasbon.entity.Pengajuan;
 import kasbon.service.AplikasiKasbonService;
 
 /**
@@ -22,12 +23,29 @@ public class PengajuanController {
     private AplikasiKasbonService aplikasiKasbonService;
 
     private List<Karyawan> daftarKaryawan;
+    private Pengajuan pengajuan = new Pengajuan();
 
     public List<Karyawan> getDaftarKaryawan() {
         if(daftarKaryawan == null){
             daftarKaryawan = aplikasiKasbonService.findAllKaryawan();
         }
         return daftarKaryawan;
+    }
+    
+    public String simpan(){
+        System.out.println("Karyawan yang dipilih : "
+                +pengajuan.getPemohon().getNip()
+                +" mengajukan Rp. "
+                +pengajuan.getNilaiPinjaman());
+        return "form?faces-redirect=true";
+    }
+
+    public Pengajuan getPengajuan() {
+        return pengajuan;
+    }
+
+    public void setPengajuan(Pengajuan pengajuan) {
+        this.pengajuan = pengajuan;
     }
     
     /** Creates a new instance of PengajuanController */
